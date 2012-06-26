de3c46e1b63bf6a2d16ae24e8439938ec75d373b
Brett Buddin <brett@intraspirit.net>
2012-06-25 20:01:38 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Londongram::Application.config.secret_token = '75d717f0c268741ce9187a75e8cdf90f8362dcff51e9a3ac112950e3585b3a3062b0ef11c9cb1a6a0c8fa1a3d01df742322928ac8930b5549f919a70684a39a8'
